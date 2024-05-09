b00d7aa93a07d2f511be0a0944483317d74807ab
Sith Traly <sithtraly147@gmail.com>
2024-05-09 09:49:23 +0700

    add post method

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +6,12 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(@Query() query: any): string {
+    return this.appService.getHello() + ' ' + query.id;
+  }
+
+  @Post()
+  postHello() {
+    return 'This is post method';
   }
 }
